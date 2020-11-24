5a4a75f106473d39f982d0a3cf1e41a54b6a9770
birolezgin <b.ezginksf@gmail.com>
2020-11-23 19:57:57 -0800

    API.js/utils added

---
+++ client/src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// The getSongs method retrieves songs from the server
+// It accepts a "query" or term to search the song api for
+export default {
+  getSongs: function(query) {
+    return axios.get("/api/songs", { params: { q: query } });
+  }
+};
