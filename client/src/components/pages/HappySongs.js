09c7e78cbdbea38ae6cb1c8602918eb2265df8a4
birolezgin <b.ezginksf@gmail.com>
2020-11-23 20:22:44 -0800

    pages added

---
+++ client/src/components/pages/HappySongs.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./pages.css";
+
+function HappySongs() {
+    return (
+        <div className="container">
+                    <div className="card">
+                    <div className = "card-body"></div>
+                    <h2 className="card-title">Happy Songs</h2><br></br>
+                
+                    <iframe width="530" height="335"
+                    src="https://www.youtube.com/embed/vP_Bi4z65Wk">
+                    </iframe><br></br>
+                    <iframe width="530" height="335"
+                    src="https://www.youtube.com//embed/2zToEPpFEN8">
+                    </iframe><br></br>
+                    </div>
+        </div>
+          
+    )
+}
+export default HappySongs;
