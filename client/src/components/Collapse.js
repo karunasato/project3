33925568ecd7cdd380a5747ce23f61a9c2f7aecb
Karuna Sato <karutimes2@comcast.net>
2020-12-04 19:50:24 -0800

    removed card on collapse.js

--- client/src/components/Collapse.js
+++ client/src/components/Collapse.js
@@ -2,17 +2,15 @@ import React from "react";
 
 export default ()=> 
 <div className="accordion" id="accordionExample">
-<div className="card">
-  <div className="card-header" id="headingOne">
+
     <h2 className="mb-0">
       <button className="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
         How Does it Work?
       </button>
     </h2>
-  </div>
 
   <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
-    <div class="card-body">
+
      <h3>Mood Swing plays songs based on your mood!</h3> 
      <br/>
       <p>Are you happy?- Give a thumbs-up on the webcam and we'll play you happy songs!</p>
@@ -20,5 +18,5 @@ export default ()=>
       <p>Are you in love? Partying? Needing some rest?- click on the buttons below the webcam and you'll get songs that will coordinate.</p>
     </div>
   </div>
-</div>
-</div>
+
+
